3b10a0f97ad4b7991e019acd3d3204cf78ef8752
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-07 15:09:20 -0300

    feat: txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        message = "Formato inválido"
+    elif not os.path.exists(path_file):
+        message = f"Arquivo {path_file} não encontrado"
+    else:
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+
+    print(message, file=sys.stderr)
+    return None
