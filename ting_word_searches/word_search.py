3734e733e7acd10b19a58b7b5d845eaa5c5aad64
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-09 18:14:06 -0300

    feat: word search - exists word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    word_counter = []
+    qsize = len(instance)
+    count_line = 0
+
+    for i in range(qsize):
+        file = instance.search(i)
+        occurrences = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for line in file["linhas_do_arquivo"]:
+            count_line += 1
+            if word.lower() in line.lower():
+                occurrences["ocorrencias"].append({"linha": count_line})
+
+        if len(occurrences["ocorrencias"]) > 0:
+            word_counter.append(occurrences)
+
+    return word_counter
 
 
 def search_by_word(word, instance):
