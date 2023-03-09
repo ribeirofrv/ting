25566e55eae89ce941b2e961c001df1918547d4a
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-03-08 21:01:00 -0300

    feat: file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,29 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+
+    for item in range(len(instance)):
+        file = instance.search(item)["nome_do_arquivo"]
+        if file == path_file:
+            return
+
+    lines = txt_importer(path_file)
+    num_lines = len(lines)
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": num_lines,
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(file_data)
+    print(file_data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    pass
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    pass
